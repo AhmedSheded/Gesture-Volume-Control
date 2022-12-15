0633806e70b5b19b9d4cd4f7dc9e6b14b41e695d
Ahmed Sheded <sheded222@gmail.com>
2022-12-15 21:05:57 +0200

    hand tracking module

--- HandTracking.py
+++ HandTracking.py
@@ -6,11 +6,30 @@ cap = cv.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
+    frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    results = hands.process(frameRGB)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w = frame.shape[:2]
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                # if id == 0:
+                #     cv.circle(frame, (cx, cy), 20, (255, 255, 0), cv.FILLED)
 
+            mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
 
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv.putText(frame, "FPS "+str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 2)
 
     cv.imshow('frame', frame)
     if cv.waitKey(1) == 27:
---
+++ HandTrackingModule.py
@@ -0,0 +1,68 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+
+class HandDetector():
+    def __init__(self, mode=False, MaxHands=2, complexity=1, detectionConf=0.5, trackConf=0.5):
+        self.mode=mode
+        self.MaxHands=MaxHands
+        self.complexity=complexity
+        self.detectionConf=detectionConf
+        self.trackConf=trackConf
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.MaxHands, self.complexity, self.detectionConf, self.trackConf)
+        self.mpDraw = mp.solutions.drawing_utils
+
+
+    def findHands(self, frame, draw=True):
+        frameRGB = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        self.results = self.hands.process(frameRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(frame, handLms, self.mpHands.HAND_CONNECTIONS)
+        return frame
+
+    def findPosition(self, frame, handNo=0, draw=False):
+        lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w = frame.shape[:2]
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv.circle(frame, (cx, cy), 15, (255, 255, 0), cv.FILLED)
+
+        return lmList
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv.VideoCapture(0)
+    detector = HandDetector()
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        frame=detector.findHands(frame)
+        lmList = detector.findPosition(frame)
+
+        if len(lmList) !=0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv.putText(frame, "FPS "+str(int(fps)), (10, 70), cv.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 2)
+
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == 27:
+            break
+
+
+if __name__ == '__main__':
+    main()
