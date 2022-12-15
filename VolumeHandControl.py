5de52a810d7a0b426de851a37bb16801766a95d5
Ahmed Sheded <sheded222@gmail.com>
2022-12-15 23:49:30 +0200

    modify volume control

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -2,6 +2,11 @@ import cv2 as cv
 import time
 import numpy as np
 import HandTrackingModule as hm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 wCam, hCam = 640, 480
 pTime = 0
@@ -14,6 +19,22 @@ cap.set(4, hCam)
 
 detector = hm.HandDetector(detectionConf=0.7)
 
+
+
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+# volume.GetMute()
+# volume.GetMasterVolumeLevel()
+print(volume.GetVolumeRange())
+# volume.SetMasterVolumeLevel(-20.0, None)
+
+
+
+
 while cap.isOpened():
     ret, frame = cap.read()
 
@@ -24,10 +45,17 @@ while cap.isOpened():
         x1, y1 = points[4][1], points[4][2]
         x2, y2 = points[8][1], points[8][2]
         cx, cy = (x1+x2)//2, (y1+y2)//2
-        cv.circle(frame, (x1, y1), 10, (50, 0, 255), cv.FILLED)
-        cv.circle(frame, (x2, y2), 10, (50, 0, 255), cv.FILLED)
-        cv.circle(frame, (cx, cy), 10, (50, 0, 255), cv.FILLED)
-        cv.line(frame, (x1, y1), (x2, y2), (50, 0, 255), 3)
+        color = (255, 0, 255)
+        cv.circle(frame, (x1, y1), 10, color, cv.FILLED)
+        cv.circle(frame, (x2, y2), 10, color, cv.FILLED)
+        cv.circle(frame, (cx, cy), 10, color, cv.FILLED)
+        cv.line(frame, (x1, y1), (x2, y2), color, 3)
+
+        length = math.hypot(x2-x1, y2-y1)
+        print(length)
+        if length<50:
+            cv.circle(frame, (cx, cy), 10, (0, 255, 0), cv.FILLED)
+
 
 
     cTime = time.time()
